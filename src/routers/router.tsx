097e431883938dc56c38c0e14907cbb34f03b9f2
eunchu <lovely-ej01@nate.com>
2022-01-09 22:55:51 +0900

    change path

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -9,7 +9,7 @@ const RouterConfig = () => {
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="main" element={<Layer />}>
+        <Route path="/sknetzero" element={<Layer />}>
           <Route index element={<MainPage />} />
           {/* <Route path="community" element={<CommunityPage />} />
           <Route path="membership" element={<div>멤버십 페이지</div>} />
